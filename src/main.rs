196788e488a3b74710b29f74d06ce9f61466bb94
TEESDAAL <alanteesdale@proton.me>
2023-03-23 23:18:45 +1300

    Revert "First commit"
    This reverts commit 3973c158629587184dd2dfa85ded066bd7bc8562.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use image::{io::Reader as ImageReader, DynamicImage, GenericImageView};
-use std::collections::HashMap;
 use std::env;
 
 fn main() {
@@ -12,55 +11,33 @@ fn main() {
     };
     let img = ImageReader::open(path).unwrap().decode().unwrap();
 
-    print!("{}", get_image(img, 10));
+    print!("{}", get_image(img, 4));
 }
 
 fn get_image(img: DynamicImage, scale: u32) -> String {
-    let mut emoji_art = String::new();
+    let mut ascii_art = String::new();
     for y in 0..img.height() {
         for x in 0..img.width() {
             if y % (scale * 2) != 0 || x % scale != 0 {
                 continue;
             }
             let pixel = img.get_pixel(x, y);
-            let rgb = [pixel[0], pixel[1], pixel[2]];
-            emoji_art.push(get_str_emoji(rgb));
+            let strength = ((pixel[0] as f32 / 3. + pixel[1] as f32 / 3. + pixel[2] as f32 / 3.)
+                as f32
+                * (pixel[3] as f32 / 255.))
+                .round() as u8;
+            ascii_art.push(get_str_ascii(strength));
         }
         if y % (scale * 2) == 0 {
-            emoji_art.push('\n');
+            ascii_art.push('\n');
         }
     }
-    emoji_art
+    ascii_art
 }
 
-fn get_str_emoji(rgb: [u8; 3]) -> char {
-    let emoji = HashMap::from([
-        ([244, 65, 51], '🟥'),
-        ([255, 205, 46], '🟨'),
-        ([18, 119, 211], '🟦'),
-        ([172, 69, 189], '🟪'),
-        ([255, 153, 0], '🟧'),
-        ([184, 109, 83], '🟫'),
-        ([64, 64, 64], '⬛'),
-        ([225, 225, 225], '⬜'),
-        ([125, 180, 64], '🟩'),
-    ]);
+fn get_str_ascii(strength: u8) -> char {
+    let characters = [' ', '.', '`', ',', '-', '~', '+', 'o', 'w', 'm', '%', '@'];
+    let index = ((characters.len() - 1) as f32 * strength as f32 / 255.).round() as usize;
 
-    let square_colors = emoji.keys().collect::<Vec<_>>();
-    let mut closest_rgb = square_colors[0];
-    let mut diff = difference(rgb, *square_colors[0]);
-    for i in 1..square_colors.len() {
-        let new_diff = difference(rgb, *square_colors[i]);
-        if new_diff <= diff {
-            closest_rgb = square_colors[i];
-            diff = new_diff;
-        }
-    }
-    return emoji[closest_rgb];
-}
-
-fn difference(rgb1: [u8; 3], rgb2: [u8; 3]) -> u32 {
-    return rgb1[0].abs_diff(rgb2[0]) as u32
-        + rgb1[1].abs_diff(rgb2[1]) as u32
-        + rgb1[2].abs_diff(rgb2[2]) as u32;
+    return characters[index as usize];
 }
